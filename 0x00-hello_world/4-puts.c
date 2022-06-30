ddd9da81e85163a912eb3060757a83231f5d3fc4
puffdog <mabasagiftpd@gmail.com>
2022-06-30 12:19:34 -0700

    ptss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main is the entry point
  * Return: 0 is the result
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like bulding multilingual puzzle");
-        return (0);
+        return(0);
 }
