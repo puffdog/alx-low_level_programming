1dc1fa619f0874f496368b1433fe8d883c3bf634
puffdog <mabasagiftpd@gmail.com>
2022-07-01 16:42:25 -0700

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("the number is positive %d\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("the number is zero %d\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("the number is negative %d\n", n);
+printf("%d is negative\n", n);
 }
 /* your code goes there */
 return (0);
