d5a906a76b289539b9de7984748cefd0680b166e
puffdog <mabasagiftpd@gmail.com>
2022-07-01 18:43:47 -0700

    int

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - is the entry point
+ * Description:defines cod
+ * Return:success 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
