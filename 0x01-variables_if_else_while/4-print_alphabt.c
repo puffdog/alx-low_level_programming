7f61925020789b73d9d95269bf87c200ed602fd1
puffdog <mabasagiftpd@gmail.com>
2022-07-01 18:26:32 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+/**
+ * main - is the entry point
+ * Description:describe structure of code
+ * Return:success 0
+ */
+int main(void)
+{
+char low;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
+}
