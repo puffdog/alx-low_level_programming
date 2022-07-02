614926f50d9ca25cd66846866407e16a73e2c52e
puffdog <mabasagiftpd@gmail.com>
2022-07-01 20:05:43 -0700

    yey

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - is the entry point
+ * Description:does it all
+ * Return:success 0
+ */
+int main(void)
+{
+int m;
+for (m = '0', m <= '9'; m++)
+{
+putchar(m);
+if (m != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
