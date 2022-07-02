7bca849a983ed416c3c2eee42e4e3113fe9e3503
puffdog <mabasagiftpd@gmail.com>
2022-07-01 18:14:11 -0700

    putchar twoo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - is the entry point
+ * Description:describe what code does
+ * Return: success 0
+ */
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n')
+return (0);
+}
