ca69058b93ec59e930f62bf258c2621bd61976c9
puffdog <mabasagiftpd@gmail.com>
2022-07-01 19:15:47 -0700

    yoh alx dziawa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - is the entry point
+ * Description:is everything
+ * Return:success or failer 0
+ */
+int main(void)
+{
+int g;
+char low;
+for (g = '0'; g <= '9'; g++)
+putchar(g);
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
+putchar('\n')
+return (0);
+}
