c4432f558234193c4964d6c09eca2ebf30ef7c67
puffdog <mabasagiftpd@gmail.com>
2022-07-05 12:34:09 -0700

    putchsr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#inlucde <stdio.h>
+
+/*
+ * main - is the entry point
+ * Description:describe what code does
+ * Return:success 0 
+ */
+int main(void)
+printf(_putchar\n);
+return 0;
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+read me for functions and nested loops in C
