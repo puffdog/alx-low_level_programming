bc0684909c84a7bcbd86ad4c4955991f2a334ca1
puffdog <mabasagiftpd@gmail.com>
2022-07-06 03:08:21 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - is the main entry
+ *@a: is the number value
+ *Description: This function returns absolute value of a number
+ *Return: return number of a
+ */
+int _abs(int a)
+
+{
+
+if (a >= 0)
+return (a);
+else
+return (a * -1);
+}
