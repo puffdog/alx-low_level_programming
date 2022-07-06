f933ee66d594040557c7b06bc8882f7cdb9eb07c
puffdog <mabasagiftpd@gmail.com>
2022-07-06 07:34:05 -0700

    adddd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - in main
+ * @a: as nteger
+ * @b: as integer
+ * Description:we are adding two values
+ * Return: success
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ int _abs(int a);
 int print_last_digit(int b);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 #endif
