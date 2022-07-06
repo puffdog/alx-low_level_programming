1e1e30e12dd690ec63a97854255d9e4ecf471abe
puffdog <mabasagiftpd@gmail.com>
2022-07-05 17:47:55 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _islower - is found on the main
+ *@c:is the character
+ *Description:describe your code 
+ *Return:return 1 if is correct or 0 if incorrect
+ */
+int _islower(int c)
+
+{
+
+		char i;
+
+			int lower = 0;
+
+
+
+				for (i = 'a'; i <= 'z'; i++)
+
+						{
+
+									if (i == c)
+
+													lower = 1;
+
+										}
+
+
+
+					return (lower);
+
+}
