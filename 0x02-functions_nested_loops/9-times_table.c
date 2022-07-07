785fad8c3779ea7e41d7f4b4f1b02fd86de5bac7
puffdog <mabasagiftpd@gmail.com>
2022-07-06 19:43:46 -0700

    yeag

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,69 @@
  * Return: Nothing 0
  */
 void times_table(void)
+
 {
-int i, j, n;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-n = i * j;
-{
-if ((n / 10) == 0)
-{
-if (j != 0)
-_putchar(' ');
-_putchar(n + '0');
-if (j == 9)
-continue;
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-if (j == 9)
-continue;
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
-}
+
+	        int i = 0;
+
+		        int j = 0;
+
+			        int prod;
+
+				 
+
+				        while (i < 10)
+
+						        {
+
+								                while (j < 10)
+
+											                {
+
+														                        prod = i * j;
+
+																	                        if (prod <= 9 && j != 0)
+
+																					                                _putchar(' ');
+
+																				                        if (prod > 9)
+
+																								                        {
+
+																												                                _putchar(prod / 10 + '0');
+
+																																                                _putchar(prod % 10 + '0');
+
+																																				                        }
+
+																							                        else
+
+																											                        {
+
+																															                                _putchar(prod + '0');
+
+																																			                        }
+
+																										                        if (j != 9)
+
+																														                        {
+
+																																		                                _putchar(',');
+
+																																						                                _putchar(' ');
+
+																																										                        }
+
+																													                        j++;
+
+																																                }
+
+										                _putchar('\n');
+
+												                i++;
+
+														                j = 0;
+
+																        }
+
 }
