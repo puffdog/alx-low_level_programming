9febdcce74ec383e18fde7f57d744677d201d05e
puffdog <mabasagiftpd@gmail.com>
2022-07-12 13:52:15 -0700

    backweards

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - is the main function
+ * @s: is the variable
+ * Return: return success 0
+ */
+
+void print_rev(char *s)
+{
+int a = 0;
+while (s[a] != '\n')
+a++;
+while (a)
+_putchar(s[--a]);
+_putchar('\n');
+}
