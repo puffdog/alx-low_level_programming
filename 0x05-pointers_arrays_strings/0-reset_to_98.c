ec707d14b30954dd843b5573e13bbaca8f6c8cde
puffdog <mabasagiftpd@gmail.com>
2022-07-12 01:32:18 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - is my main function
+ * @n: is the varialbe
+ * Return: 0 if success
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo is about pointers and arrays well in short we use pointers to determine the adrress of variable by using *p or any value instead of p
