d58a8256ec36140211d7c2e9bc91308c38157732
puffdog <mabasagiftpd@gmail.com>
2022-07-12 14:25:34 -0700

    reverseestring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - is the function
+ * @s: is the variable
+ * Return: sucees 0
+ */
+
+void rev_string(char *s)
+{
+int a = 0, b = 0;
+char rev;
+while (s[a] != '\0')
+a++;
+while (a > b--)
+{
+rev = s[b];
+s[b++] = s[a];
+s[a] = rev;
+}
+}
