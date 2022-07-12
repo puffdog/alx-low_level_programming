3638508a67a21b6b5271d189869ee0252c1e4ab1
puffdog <mabasagiftpd@gmail.com>
2022-07-12 16:14:25 -0700

    null

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,12 @@
  * *_strcpy - is main func
  * @dest: is tghe var
  * @src: is the var
- * Return: success 0
+i * Return: success 0
  */
 char *_strcpy(char *dest, char *src)
 {
 char *a = dest;
-while (*src)
+while (*src != '\0')
 *dest++ = *src++;
 return (a);
 }
