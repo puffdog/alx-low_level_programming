24de120d695eced3e1e85fa45f9f13b71d93eb27
puffdog <mabasagiftpd@gmail.com>
2022-07-12 02:38:01 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - is the function
+ * @a: is the variable
+ * @b: is the variable
+ * Return: return 0 if success
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+*a = 98;
+b = 42;
+c = *a;
+*a = *b;
+*b = c;
+}
