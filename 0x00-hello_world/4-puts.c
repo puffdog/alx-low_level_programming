e747e1ea5ad1d834d861bee035685369db9d30f9
puffdog <mabasagiftpd@gmail.com>
2023-12-14 16:45:50 -0800

    my first code i used puts to print inseat of printf and putchar the different is puts print strings to the standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - this is the entry point
+ *
+ * Return: always zero if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
