975a31f3dfc38f719646ddbe02b0efe2b423d7cf
puffdog <mabasagiftpd@gmail.com>
2023-12-14 17:54:31 -0800

    here am printing size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int e;
 	float f;
 
-	printf("Size of an char is: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int is: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of an long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an long long int is: %lu byte(s)\n", (unsigned long)sizeof(e));
-	printf("Size of an float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char is: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a int is: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
